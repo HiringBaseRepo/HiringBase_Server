fd459fbe6cafa6ed172c99c6aa8475c2f8228629
Wildann77 <boyblanco77@gmail.com>
2026-05-04 11:24:03 +0700

    feat: add UPLOAD_PREFIX_COMPANY_ASSET and UPLOAD_PREFIX_JOB_ATTACHMENT constants while removing UPLOAD_PREFIX_CV

--- app/shared/constants/storage.py
+++ app/shared/constants/storage.py
@@ -2,6 +2,7 @@
 
 MAX_FILE_SIZE_MB = 10
 ALLOWED_EXTENSIONS = {"pdf", "jpg", "jpeg", "png", "doc", "docx"}
-UPLOAD_PREFIX_CV = "cvs"
 UPLOAD_PREFIX_DOCUMENT = "documents"
 UPLOAD_PREFIX_PORTFOLIO = "portfolios"
+UPLOAD_PREFIX_COMPANY_ASSET = "company-assets"
+UPLOAD_PREFIX_JOB_ATTACHMENT = "job-attachments"
